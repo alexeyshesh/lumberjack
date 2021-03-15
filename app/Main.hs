047769dc5babbb4c81d4fb650f14d0096ce439f1
Alexey Sheshukov <alexeyshesh@yandex.ru>
2021-03-16 01:28:38 +0300

    Updated code structure, added branch drawer and undefined functions signatures

--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,30 @@ import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Data.Bitmap
 import System.Random 
 
--- Data types
 
-data Direction = LeftSide | RightSide
+-- Data types 
 
-data GameState = GameState { 
-    score :: Int,
-    alive :: Bool,
-    randomGen :: StdGen,
-    nextBranches :: [Direction],
-    curSide :: Direction,
-    active :: Bool,
+data Direction = LeftSide | RightSide | Center
+
+data Assets = Assets {
     playerLeft :: Picture,
     playerLeftActive :: Picture,
     playerRight :: Picture,
     playerRightActive :: Picture,
-    background :: Picture
+    background :: Picture,
+    branchLeft :: Picture,
+    branchRight :: Picture
+}
+
+data GameState = GameState { 
+    score :: Int,
+    alive :: Bool,
+    time :: Float,
+    randomGen :: StdGen,
+    nextBranches :: [Direction],
+    curSide :: Direction,
+    active :: Bool,
+    assets :: Assets
 }
 
 -- Game Display Mode
@@ -41,11 +49,43 @@ fps = 60
 -- Draw world
 
 drawApp :: GameState -> Picture 
-drawApp (GameState s _ _ _ LeftSide False pl pla pr pra bg) = Pictures [bg, pl]
-drawApp (GameState s _ _ _ RightSide False pl pla pr pra bg) = Pictures [bg, pr]
-drawApp (GameState s _ _ _ LeftSide True pl pla pr pra bg) = Pictures [bg, pla]
-drawApp (GameState s _ _ _ RightSide True pl pla pr pra bg) = Pictures [bg, pra]
+drawApp (GameState s _ _ _ branches side active assets) = Pictures [bg, player, brs]
+    where 
+        bg = background assets
+        player = drawPlayer side active assets
+        brs = drawBranches branches assets
 
+drawPlayer :: Direction -> Bool -> Assets -> Picture 
+drawPlayer LeftSide False = playerLeft 
+drawPlayer LeftSide True = playerLeftActive 
+drawPlayer RightSide False = playerRight 
+drawPlayer RightSide True = playerRightActive 
+
+drawBranches :: [Direction] -> Assets -> Picture 
+drawBranches dir assets = Pictures(drawBranchesHelper 0.0 dir assets)
+
+drawBranchesHelper :: Float -> [Direction] -> Assets -> [Picture]
+drawBranchesHelper _ [] _ = []
+drawBranchesHelper shift (LeftSide:dir) assets = 
+    Translate 0 shift (branchLeft assets) : drawBranchesHelper (shift + 60) dir assets
+drawBranchesHelper shift (RightSide:dir) assets = 
+    Translate 0 shift (branchRight assets) : drawBranchesHelper (shift + 60) dir assets
+drawBranchesHelper shift (Center:dir) assets = 
+    drawBranchesHelper (shift + 60) dir assets
+
+drawScore :: Int -> Assets -> Picture 
+drawScore = undefined
+
+drawTime :: Float -> Picture 
+drawTime = undefined
+
+-- World generator 
+
+defaultBranches :: [Direction]
+defaultBranches = [Center, LeftSide, Center, RightSide, Center, RightSide]
+
+branchGenerator :: StdGen -> [Direction] -> GameState
+branchGenerator = undefined 
 
 -- Handle events
 
@@ -72,18 +112,25 @@ main = do
     pr <- loadBMP "assets/pr.bmp"
     pra <- loadBMP "assets/pra.bmp"
     bg <- loadBMP "assets/bg.bmp"
+    bl <- loadBMP "assets/bl.bmp"
+    br <- loadBMP "assets/br.BMP"
 
     let initState = GameState {
         score=0, 
         alive=True, 
+        time = 20,
         randomGen=gen, 
-        nextBranches=[], 
+        nextBranches=defaultBranches, 
         curSide=LeftSide, 
         active=False,
-        playerLeft=pl, 
-        playerLeftActive=pla,
-        playerRight=pr,
-        playerRightActive=pra,
-        background=bg
+        assets = Assets {
+            playerLeft=pl, 
+            playerLeftActive=pla,
+            playerRight=pr,
+            playerRightActive=pra,
+            background=bg,
+            branchLeft=bl,
+            branchRight=br
+        }
     }
-    play display (makeColorI 72 219 251 255) fps initState drawApp handleEvent updateApp
+    play display black fps initState drawApp handleEvent updateApp
